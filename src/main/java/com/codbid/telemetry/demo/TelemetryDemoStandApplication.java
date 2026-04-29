272f0aeb5c7743e7f2b7577aefa69b2a82f1c8a2
Daniil Istomin <istomin.05.3@gmail.com>
2026-04-29 22:43:44 +0700

    Initial commit

---
+++ src/main/java/com/codbid/telemetry/demo/TelemetryDemoStandApplication.java
@@ -0,0 +1,13 @@
+package com.codbid.telemetry.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryDemoStandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelemetryDemoStandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codbid/telemetry/demo/TelemetryDemoStandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codbid.telemetry.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetryDemoStandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
